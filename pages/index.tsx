7bc477ef9b884a275fffde6a02e954a3b5a6622f
Liedel <liedel75@gmail.com>
2022-07-12 17:19:19 +0200

    add header & icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,22 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>Netflix - Accueil -</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      Lets build Netflix
-
+    <Header/>
     <main>
-
+      {/* row */}
+      {/* row */}
+      {/* row */}
+      {/* row */}
+      {/* row */}
+      {/* row */}
     </main>
 
     </div>
