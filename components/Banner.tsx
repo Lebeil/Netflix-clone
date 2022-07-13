1579e8572500fd3d0126acd6afb028c1f3d87e62
Liedel <liedel75@gmail.com>
2022-07-13 22:12:14 +0200

    add banner image & domain image

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Movie } from '../typings';
+import Image from 'next/image'
+import { baseUrl } from '../constants/movie';
 
-const Banner = () => {
+interface Props {
+    netflixOrignals: Movie[]
+}
+
+const Banner = ({netflixOrignals}: Props) => {
+    const [movie, setMovie] = useState<Movie | null>(null);
+    console.log(netflixOrignals);
+    
+    // picture random
+    useEffect(()=> {
+        setMovie(netflixOrignals[Math.floor(Math.random() * netflixOrignals.length)])
+    }, [netflixOrignals])
+
     return (
         <div>
             <div>
-                <h1>Banner</h1>
+                <Image
+                    src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+                    layout='fill'
+                />
             </div>
         </div>
     );
